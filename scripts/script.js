4d9c2755bdff8220da3b1526ef934fa8501471d4
Tomasz Stępnik <Tomasz.Stepnik77@gmail.com>
2023-10-30 19:44:46 +0100

    move folders

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,6 @@ var numOfClicks = 0;
 $(document).ready(function () {
   $("#click-button").on("click", function () {
     numOfClicks += 5;
-    document.getElementById("click-counter").innerHTML = numOfClicks;
+    document.getElementById("click-counter.").innerHTML = numOfClicks;
   });
 });
